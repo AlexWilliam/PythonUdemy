c4e13c852a3234bacdecaa76cd76cc7ba4bc4bf8
Alex Goncalves <alex.goncalves@emedrs.local>
2018-10-18 12:37:33 -0300

    Aula 26 udemy

---
+++ excript/app-comerciais-kivy/aulas/aula26.py
@@ -0,0 +1,19 @@
+num_int = 1
+num_dec = 7.5
+val_str = "qualquer texto"
+
+print("O valor é:", num_int)
+print("O valor é: %i" %num_int)
+print("O valor é: " + str(num_int))
+
+print("")
+
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.2f" %num_dec)
+print("Concatenando decimal:" + str(num_dec))
+
+print("")
+
+print("Concatenando string:", val_str)
+print("Concatenando string: %s" %val_str)
+print("Concatenando stringgir a: " + val_str)
