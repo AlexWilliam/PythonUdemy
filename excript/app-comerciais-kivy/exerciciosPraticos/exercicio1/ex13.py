202f0baa4d3a79188c12e7f5cb0739e8939b4d75
AlexWilliam <alex.william@outlook.com>
2019-07-19 12:41:59 -0300

    Exercícios finalizados

--- excript/app-comerciais-kivy/exerciciosPraticos/exercicio1/ex13.py
+++ excript/app-comerciais-kivy/exerciciosPraticos/exercicio1/ex13.py
@@ -1,4 +1,14 @@
+dias=int(input("Informe o número de dias:"))
 hora_total=input("Digite a hora(HH:mm:ss): ")
+'''
+Caso queira a hora separada use este codigo
+hora=int(input("Digite as horas:"))
+minuto=int(input("Digite os minutos:"))
+segundos=int(input("Digite os segundos:"))
+'''
+
+
+dias_em_segundos=(24*(60**2))
 
 hora_quebrada=hora_total.split(":")
 
@@ -6,6 +16,6 @@ hora=int(hora_quebrada[0])
 minuto=int(hora_quebrada[1])
 segundos=int(hora_quebrada[2])
 
-totalSegundos=(hora*(60**2))+(minuto*60)+segundos
+totalSegundos=dias_em_segundos+(hora*(60**2))+(minuto*60)+segundos
 
 print("Total de segundos da hora", hora_total,"é",totalSegundos)
