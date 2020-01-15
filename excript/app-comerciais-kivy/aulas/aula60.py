b00c30bb31299c2a86ec14ec74dd206ecefe0a9d
AlexWilliam <alex.william@outlook.com>
2020-01-15 13:33:14 -0300

    Aulas dia 15-01-2020

---
+++ excript/app-comerciais-kivy/aulas/aula60.py
@@ -0,0 +1,20 @@
+#x[, ]y, z
+
+a = list(range(0, 10, 2))
+
+for c in a:
+    print(c)
+
+print("---")
+
+b = list(range(0, 10, 1))
+
+for c in b:
+    print(c)
+
+print("---")
+
+d = list(range(10))
+
+for c in d:
+    print(c)
