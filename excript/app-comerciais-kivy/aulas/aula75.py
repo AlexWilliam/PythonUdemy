bb79b2a696085b78a0f9a9fe45e2b7bf313babf3
AlexWilliam <alex.william@outlook.com>
2020-03-10 17:19:20 -0300

    Aulas

---
+++ excript/app-comerciais-kivy/aulas/aula75.py
@@ -0,0 +1,10 @@
+# Forma tradicional
+t = tuple('abc')
+
+t2 = 'aaa', 1, True
+
+# Nao pode adicionar, remover ou alterar objetos tupla
+# Objetos tupla, sao READONLY
+# t2.append('xxx')
+
+print(type(t2))
